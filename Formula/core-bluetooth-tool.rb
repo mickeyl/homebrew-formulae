98c5935bafe3bf3357adab4817b6c9a731a41f79
Dr. Michael Lauer <mickey@vanille-media.de>
2021-03-22 16:19:28 +0100

    ++head

--- Formula/core-bluetooth-tool.rb
+++ Formula/core-bluetooth-tool.rb
@@ -6,7 +6,10 @@ class CoreBluetoothTool < Formula
   desc "Bluetooth Low Energy Tool for macOS"
   homepage "https://github.com/mickeyl/core-bluetooth-tool"
   url "https://github.com/mickeyl/core-bluetooth-tool.git", tag: "0.1.0", revision: "b3cf8c441cc26dcd3ddc906e6052df5f953fa978"
+
   license "MIT"
+
+  head "https://github.com/mickeyl/", shallow: false
 
   depends_on xcode: ["12.0", :build]
 
