84a66c526a51cbfd323a06b3162d069de9e56774
Dr. Michael Lauer <mickey@vanille-media.de>
2021-03-22 16:02:20 +0100

    initial commit

---
+++ Formula/core-bluetooth-tool.rb
@@ -0,0 +1,20 @@
+# typed: false
+# frozen_string_literal: true
+
+# :nodoc:
+class CoreBluetoothTool < Formula
+  desc "Bluetooth Low Energy Tool for macOS"
+  homepage "https://github.com/mickeyl/core-bluetooth-tool"
+  url "https://github.com/mickeyl/core-bluetooth-tool.git", tag: "0.1.0", revision: "b3cf8c441cc26dcd3ddc906e6052df5f953fa978"
+  license "MIT"
+
+  depends_on xcode: ["12.0", :build]
+
+  def install
+    system "make", "install", "prefix=#{prefix}"
+  end
+
+  test do
+    system bin/"core-bluetooth-tool", "--help"
+  end
+end
---
+++ README.md
@@ -0,0 +1,20 @@
+# mickeyl Homebrew Formulae
+
+```terminal
+$ brew tap mickeyl/formulae
+```
+
+## Currently On Tap
+
+- **[core-bluetooth-tool][core-bluetooth-tool]**:
+  A command line tool for Bluetooth Low Energy (BLE) devices.
+
+## License
+
+[The Unlicense](http://unlicense.org)
+
+## Contact
+
+mickeyl ([@mickeyl](https://twitter.com/DrMickeyLauer))
+
+[core-bluetooth-tool]: https://github.com/mickeyl/core-bluetooth-tool
