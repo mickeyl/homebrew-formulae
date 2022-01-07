554b031b23b918941236a7703b70265e59f164d5
Dr. Michael Lauer <mickey@vanille-media.de>
2022-01-07 12:05:33 +0100

    ELMpersonator: bump to 0.5.2

--- Formula/ELMpersonator.rb
+++ Formula/ELMpersonator.rb
@@ -5,7 +5,7 @@
 class Elmpersonator < Formula
   desc "ELM327 Impersonator for macOS and Linux"
   homepage "https://github.com/Automotive-Swift/ELMpersonator"
-  url "https://github.com/Automotive-Swift/ELMpersonator.git", tag: "0.5.0", revision: "e169164e0c4d01e89819a5e90482fa00a9481bda"
+  url "https://github.com/Automotive-Swift/ELMpersonator.git", tag: "0.5.2", revision: "a24e23f6a1e54b73b182a428dbbc8263d575b096"
 
   license "MIT"
 
