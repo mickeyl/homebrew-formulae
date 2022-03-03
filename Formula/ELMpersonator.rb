c357d7051e09b19b32e3df5aec0f7032bd643b26
Dr. Michael Lauer <mickey@vanille-media.de>
2022-03-03 13:40:30 +0100

    fixlet

--- Formula/ELMpersonator.rb
+++ Formula/ELMpersonator.rb
@@ -3,7 +3,7 @@ class Elmpersonator < Formula
   homepage "https://www.vanille.de"
   url "https://git.drlauer-research.com:3000/mickey/ELMpersonator.git"
   head "https://git.drlauer-research.com:3000/mickey/ELMpersonator.git", :tag => "0.5.9"
-  version "0.5.9
+  version "0.5.9"
 
   depends_on :xcode => ["13", :build]
 
