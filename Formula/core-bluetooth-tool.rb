b7f2c4c6bd0e0113eebb2135e50e7f659441d8ca
Dr. Michael Lauer <mickey@vanille-media.de>
2025-10-01 15:47:01 +0200

    core-bluetooth-tool: add shasum

--- Formula/core-bluetooth-tool.rb
+++ Formula/core-bluetooth-tool.rb
@@ -2,6 +2,7 @@ class CoreBluetoothTool < Formula
   desc "Bluetooth Low Energy Tool for macOS"
   homepage "https://www.Vanille.de"
   url "https://github.com/mickeyl/core-bluetooth-tool/archive/refs/tags/0.4.2.tar.gz"
+  sha256 "f3844f18174b89ff120595f76d6005ce8a8c78b77e397584cd4b124b76f05053"
   version "0.4.2"
   head "https://github.com/mickeyl/core-bluetooth-tool.git", branch: "master"
 
