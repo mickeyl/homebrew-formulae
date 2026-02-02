cb3c20d5744508f74957a47e64ba5015483cc900
Dr. Michael Lauer <mickey@vanille-media.de>
2026-02-02 10:06:06 +0100

    ecuconnect-tool: update to 0.8

--- Formula/ecuconnect-tool.rb
+++ Formula/ecuconnect-tool.rb
@@ -1,9 +1,9 @@
 class EcuconnectTool < Formula
   desc "ECU Connection Tool for macOS"
   homepage "https://github.com/Automotive-Swift/Swift-CANyonero"
-  url "https://github.com/Automotive-Swift/Swift-CANyonero/archive/refs/tags/0.7.0.tar.gz"
-  version "0.7.0"
-  sha256 "7672cb6951c8a873614511fe35a0ec7e0b6e80991b844d9db7bfe6180218af45"
+  url "https://github.com/Automotive-Swift/Swift-CANyonero/archive/refs/tags/0.8.tar.gz"
+  version "0.8"
+  sha256 "1b29112b9f7d6ae537f152623230b40da24a4664f244f13a4e223febe5794561"
   license "MIT"
   head "https://github.com/Automotive-Swift/Swift-CANyonero.git", branch: "master"
 
