3eb0243398dd71b514d3952022c5f584ceac8eb7
Dr. Michael Lauer <mickey@vanille-media.de>
2026-02-25 17:02:37 +0100

    ecuconnect-tool 0.9.2 with macOS PyObjC resources

--- Formula/ecuconnect-tool.rb
+++ Formula/ecuconnect-tool.rb
@@ -3,14 +3,31 @@ class EcuconnectTool < Formula
 
   desc "ECUconnect CLI for CANyonero adapters"
   homepage "https://github.com/Automotive-Swift/Swift-CANyonero"
-  url "https://github.com/Automotive-Swift/Swift-CANyonero/archive/refs/tags/0.9.1.tar.gz"
-  version "0.9.1"
-  sha256 "61f1728b8036caa4bf4ed6d2962a84861ccf347340793acc173e2db1a67b87d6"
+  url "https://github.com/Automotive-Swift/Swift-CANyonero/archive/refs/tags/0.9.2.tar.gz"
+  version "0.9.2"
+  sha256 "58e09705440508da13a9900d515ea6fc108028fef6a36b1162c527cc01f06300"
   license "MIT"
   head "https://github.com/Automotive-Swift/Swift-CANyonero.git", branch: "master"
 
   depends_on "cmake" => :build
   depends_on "python@3.12"
+
+  on_macos do
+    resource "pyobjc-core" do
+      url "https://files.pythonhosted.org/packages/b8/b6/d5612eb40be4fd5ef88c259339e6313f46ba67577a95d86c3470b951fce0/pyobjc_core-12.1.tar.gz"
+      sha256 "2bb3903f5387f72422145e1466b3ac3f7f0ef2e9960afa9bcd8961c5cbf8bd21"
+    end
+
+    resource "pyobjc-framework-cocoa" do
+      url "https://files.pythonhosted.org/packages/02/a3/16ca9a15e77c061a9250afbae2eae26f2e1579eb8ca9462ae2d2c71e1169/pyobjc_framework_cocoa-12.1.tar.gz"
+      sha256 "5556c87db95711b985d5efdaaf01c917ddd41d148b1e52a0c66b1a2e2c5c1640"
+    end
+
+    resource "pyobjc-framework-corebluetooth" do
+      url "https://files.pythonhosted.org/packages/4b/25/d21d6cb3fd249c2c2aa96ee54279f40876a0c93e7161b3304bf21cbd0bfe/pyobjc_framework_corebluetooth-12.1.tar.gz"
+      sha256 "8060c1466d90bbb9100741a1091bb79975d9ba43911c9841599879fc45c2bbe0"
+    end
+  end
 
   resource "annotated-doc" do
     url "https://files.pythonhosted.org/packages/57/ba/046ceea27344560984e26a590f90bc7f4a75b06701f653222458922b558c/annotated_doc-0.0.4.tar.gz"
