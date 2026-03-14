d5064c1640a7e17a30ca0679da9d8c88a6af2656
Dr. Michael Lauer <mickey@vanille-media.de>
2026-03-14 14:12:49 +0100

    Update core-bluetooth-tool 0.5.2 tarball SHA
    Tarball was regenerated after adding Package.swift version constraint.

--- Formula/core-bluetooth-tool.rb
+++ Formula/core-bluetooth-tool.rb
@@ -2,7 +2,7 @@ class CoreBluetoothTool < Formula
   desc "Bluetooth Low Energy Tool for macOS"
   homepage "https://www.Vanille.de"
   url "https://github.com/mickeyl/core-bluetooth-tool/archive/refs/tags/0.5.2.tar.gz"
-  sha256 "b50fc338f92044eda1a5018b239c3f28917d2a0f8090b8b77ce7a5f63252f034"
+  sha256 "a791cd8e03657ec0e2824258b5dac5398abbb2707e3a2035604cd050c7872679"
   license "MIT"
   head "https://github.com/mickeyl/core-bluetooth-tool.git", branch: "master"
 
