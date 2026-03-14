ba91ada785635d66ea826d9e745e81df0531c426
Dr. Michael Lauer <mickey@vanille-media.de>
2026-03-14 13:16:58 +0100

    Update core-bluetooth-tool to 0.5.2
    Fixes build failure on macOS 15 with Xcode 16.3 (#8).

--- Formula/core-bluetooth-tool.rb
+++ Formula/core-bluetooth-tool.rb
@@ -1,8 +1,8 @@
 class CoreBluetoothTool < Formula
   desc "Bluetooth Low Energy Tool for macOS"
   homepage "https://www.Vanille.de"
-  url "https://github.com/mickeyl/core-bluetooth-tool/archive/refs/tags/0.5.1.tar.gz"
-  sha256 "d814ac6959f97dc5ee0ee5105efd95e8f09e3dee40956f1b0e8cbf50215856c0"
+  url "https://github.com/mickeyl/core-bluetooth-tool/archive/refs/tags/0.5.2.tar.gz"
+  sha256 "b50fc338f92044eda1a5018b239c3f28917d2a0f8090b8b77ce7a5f63252f034"
   license "MIT"
   head "https://github.com/mickeyl/core-bluetooth-tool.git", branch: "master"
 
