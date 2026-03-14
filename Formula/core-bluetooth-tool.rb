a67184af682849f748097e617133ef57356bff9b
Dr. Michael Lauer <mickey@vanille-media.de>
2026-03-14 17:28:38 +0100

    Update core-bluetooth-tool to 0.5.4

--- Formula/core-bluetooth-tool.rb
+++ Formula/core-bluetooth-tool.rb
@@ -1,8 +1,8 @@
 class CoreBluetoothTool < Formula
   desc "Bluetooth Low Energy Tool for macOS"
   homepage "https://www.Vanille.de"
-  url "https://github.com/mickeyl/core-bluetooth-tool/archive/refs/tags/0.5.3.tar.gz"
-  sha256 "cff49dd58d0727d24810e5c3f61f8808ee1f3840f94ebd9c32956dcf4a0f3504"
+  url "https://github.com/mickeyl/core-bluetooth-tool/archive/refs/tags/0.5.4.tar.gz"
+  sha256 "5e15e5248861227f11e0dcd86a170727cbf3b35e9f52c347694cfcd2c9293ca0"
   license "MIT"
   head "https://github.com/mickeyl/core-bluetooth-tool.git", branch: "master"
 
