fba7c79eff0766f7a62a99cee755bb6ea4b3cec3
Dr. Michael Lauer <mickey@vanille-media.de>
2026-05-06 13:38:59 +0200

    Update lsusd to v2.0.0

--- Formula/lsusd.rb
+++ Formula/lsusd.rb
@@ -1,22 +1,14 @@
 class Lsusd < Formula
   include Language::Python::Virtualenv
 
-  desc "List USB serial devices with their associated USB metadata"
+  desc "List USB devices with their associated USB metadata"
   homepage "https://github.com/mickeyl/lsusd"
-  url "https://github.com/mickeyl/lsusd/archive/refs/tags/v1.1.0.tar.gz"
-  version "1.1.0"
-  sha256 "cbf7d4bb49bf6b39838bcd9f0153e37e28075c6cf5f9781d9b908309991f3de4"
+  url "https://github.com/mickeyl/lsusd/archive/refs/tags/v2.0.0.tar.gz"
+  sha256 "5bf8b74a1eb1b5de7c3b60311d9ca8b9a2cd645441f62e80520a042ab3fe3d5d"
   license "MIT"
   head "https://github.com/mickeyl/lsusd.git", branch: "main"
 
   depends_on "python@3.14"
-
-  on_linux do
-    odie <<~EOS
-      lsusd is best installed via pipx on Linux:
-        pipx install lsusd
-    EOS
-  end
 
   def install
     virtualenv_install_with_resources
@@ -24,7 +16,7 @@ class Lsusd < Formula
   end
 
   test do
-    output = shell_output("#{bin}/lsusd")
-    assert_match(/USB|No USB serial devices found/, output)
+    output = shell_output(bin/"lsusd")
+    assert_match(/USB|No USB devices found/, output)
   end
 end
