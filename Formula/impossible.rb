0f5ede573cb62a54048a59cad87602a8fefab01b
Dr. Michael Lauer <mickey@vanille-media.de>
2026-05-29 12:01:27 +0200

    impossible 2.3.0

--- Formula/impossible.rb
+++ Formula/impossible.rb
@@ -1,8 +1,8 @@
 class Impossible < Formula
   desc "Real BLE hardware access from the iOS Simulator"
   homepage "https://github.com/mickeyl/ImpossiBLE"
-  url "https://github.com/mickeyl/ImpossiBLE/archive/refs/tags/2.2.1.tar.gz"
-  sha256 "84eb3fa4606dacbb2319d1305b244cf976be68f707ea792fa3ee57a36b78e458"
+  url "https://github.com/mickeyl/ImpossiBLE/archive/refs/tags/2.3.0.tar.gz"
+  sha256 "35e9b7957dc633512a608fd49eb0521b7b2f36654d85e2d8c25e1b7df60180c7"
   license "MIT"
   head "https://github.com/mickeyl/ImpossiBLE.git", branch: "master"
 
