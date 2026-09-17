4bacc1a305d230646c409d78667b1b353bf06d3f
Dr. Michael Lauer <mickey@vanille-media.de>
2026-09-17 11:50:56 +0200

    elmterm 1.2.0

--- Formula/elmterm.rb
+++ Formula/elmterm.rb
@@ -1,16 +1,16 @@
 class Elmterm < Formula
   desc "Intelligent terminal for ELM327-compatible automotive diagnostics"
   homepage "https://github.com/Automotive-Swift/ELMterm"
-  url "https://github.com/Automotive-Swift/ELMterm/archive/refs/tags/1.1.0.tar.gz"
-  version "1.1.0"
-  sha256 "3f007d50546018b3c7146251f4f36a3209399c64640bcedcea7e24a902c30f31"
+  url "https://github.com/Automotive-Swift/ELMterm/archive/refs/tags/1.2.0.tar.gz"
+  version "1.2.0"
+  sha256 "e68156d1c8f0974d987c8503c129a22e0307db18a5fb2ffbe1a8c9e04738ccd8"
   license "MIT"
   head "https://github.com/Automotive-Swift/ELMterm.git", branch: "master"
 
   depends_on :macos
 
   def install
-    system "swift", "build", "-c", "release", "--disable-sandbox"
+    system "swift", "build", "-c", "release", "--disable-sandbox", "--force-resolved-versions"
     bin.install ".build/release/ELMterm"
     (buildpath/"elmterm.1").write((buildpath/"man/ELMterm.1").read)
     system "gzip", "-n", "elmterm.1"
@@ -20,6 +20,7 @@ class Elmterm < Formula
 
   test do
     system bin/"ELMterm", "--help"
+    assert_equal version.to_s, shell_output("#{bin}/ELMterm --version").strip
     assert_path_exists man1/"ELMterm.1"
     assert_path_exists man1/"elmterm.1.gz"
   end
